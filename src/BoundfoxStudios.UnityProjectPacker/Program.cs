91049767f3db9284273fd231f8086e65b3d66537
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2021-05-05 21:40:07 +0200

    fix: better resolving of current base directory

--- src/BoundfoxStudios.UnityProjectPacker/Program.cs
+++ src/BoundfoxStudios.UnityProjectPacker/Program.cs
@@ -1,31 +1,50 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BoundfoxStudios.UnityProjectPacker
 {
-    class Program
+  class Program
+  {
+    static async Task<int> Main(string[] args)
     {
-        static async Task<int> Main(string[] args)
-        {
-            Console.WriteLine("BoundfoxStudios :: Unity Project Packer");
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine("\nThis little tool helps to create a zip archive of your Unity project for support purposes.");
-            Console.WriteLine("After packing, please upload it either directly to our Discord Server,");
-            Console.WriteLine("or, if the package is too big, upload it to WeTransfer.com");
-            Console.WriteLine();
+      Console.WriteLine("BoundfoxStudios :: Unity Project Packer");
+      Console.WriteLine("---------------------------------------");
+      Console.WriteLine("\nThis little tool helps to create a zip archive of your Unity project for support purposes.");
+      Console.WriteLine("After packing, please upload it either directly to our Discord Server,");
+      Console.WriteLine("or, if the package is too big, upload it to WeTransfer.com");
+      Console.WriteLine();
 
-            var unityProjectPacker = new UnityProjectPacker();
-            unityProjectPacker.Log += Console.WriteLine;
+      var unityProjectPacker = new UnityProjectPacker();
+      unityProjectPacker.Log += Console.WriteLine;
 
-            var currentPath = args.Length > 0 ? args[0] : AppContext.BaseDirectory;
+      var currentPath = DetermineDirectory(args);
 
-            if (string.IsNullOrWhiteSpace(currentPath))
-            {
-              Console.WriteLine("Could not determine current path.");
-              return 1;
-            }
-            
-            return (int) await unityProjectPacker.Pack(currentPath);
-        }
+      if (string.IsNullOrWhiteSpace(currentPath))
+      {
+        Console.WriteLine("Could not determine current path.");
+        return 1;
+      }
+
+      return (int) await unityProjectPacker.Pack(currentPath);
     }
+
+    private static string DetermineDirectory(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        return args[0];
+      }
+
+      var mainModule = Process.GetCurrentProcess().MainModule;
+
+      if (mainModule?.FileName != null)
+      {
+        return Path.GetDirectoryName(mainModule.FileName);
+      }
+
+      return AppContext.BaseDirectory;
+    }
+  }
 }
